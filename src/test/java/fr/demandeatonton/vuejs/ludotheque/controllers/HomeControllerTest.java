4633530008e694ba25e0c23ed1b44e1f6554f351
Alexandre Langlais <Alexandre Langlais>
2017-07-13 07:39:00 +0200

    trying to integrate bootstrap

--- src/test/java/fr/demandeatonton/vuejs/ludotheque/controllers/HomeControllerTest.java
+++ src/test/java/fr/demandeatonton/vuejs/ludotheque/controllers/HomeControllerTest.java
@@ -6,10 +6,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import fr.demandeatonton.vuejs.ludotheque.services.GameService;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(HomeController.class)
@@ -17,12 +20,12 @@ public class HomeControllerTest {
    @Autowired
    private MockMvc mockMvc;
 
-   @Autowired
-   private MockMvc mvc;
+   @MockBean
+   GameService gameServiceMock;
 
    @Test
    public void isHomePageServed() throws Exception {
-      mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+      mockMvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
             .andExpect(view().name("home"));
    }
